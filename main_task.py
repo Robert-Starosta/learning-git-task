1b764538bd8b9d91c07ba2c370e57d6162c1d78b
Robert Starosta <Robert.starosta@o2.pl>
2021-03-24 12:48:39 +0100

    Add for loop main_task.py

--- main_task.py
+++ main_task.py
@@ -2,3 +2,6 @@ shoping_list = {
    "Piekarnia": ['Chleb', 'Pączek', 'Bułki'],
    "Warzywniak": ['Marchew', 'Seler', 'Rukola'] 
 }
+product_count = 0
+for product in shoping_list:
+    print(f"Idę do {product} kupuję tu następujące rzeczy: {shoping_list.get(product)}")
