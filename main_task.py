fa1e88ca215391baae3a9cd4cd93e917dd9aff7d
Robert Starosta <Robert.starosta@o2.pl>
2021-03-25 10:27:14 +0100

    Add count shopping list

--- main_task.py
+++ main_task.py
@@ -4,4 +4,6 @@ shoping_list = {
 }
 product_count = 0
 for product in shoping_list:
-    print(f"Idę do {product} kupuję tu następujące rzeczy: {shoping_list.get(product)}")
+    print(f"Idę do {product} kupuję tu następujące rzeczy: {shoping_list.get(product.capitalize())}")
+    product_count = product_count + len(shoping_list.get(product))
+print(f"W sumie kupuję {product_count} produktów.")
